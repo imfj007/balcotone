98fb012f32a655ffe0a764c80564e7557e537b09
imfj007 <imfj007@gmail@gmail.com>
2026-03-16 10:35:30 +0500

    Fix hero text z-index

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -104,7 +104,7 @@ const Hero = (): JSX.Element => {
       <div className="grid">
         <div className="grid h-screen place-content-center">
           <div className="grid auto-rows-min place-items-center text-center">
-            <h1 className="hero-header text-7xl font-black uppercase leading-[.8] text-white md:text-[9rem] lg:text-[13rem]">
+            <h1 className="hero-header text-7xl font-black uppercase leading-[.8] text-white md:text-[9rem] lg:text-[13rem] relative z-[999]">
               <TextSplitter
                 text="Balcotone"
                 wordDisplayStyle="block"
