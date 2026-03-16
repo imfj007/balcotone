ae693c877bf3208de3f7d3fe2addfe3667fb5940
imfj007 <imfj007@gmail@gmail.com>
2026-03-16 10:41:29 +0500

    Fix image stretching

--- src/components/ProductItem.tsx
+++ src/components/ProductItem.tsx
@@ -1,14 +1,11 @@
 "use client";
-
 import { useTexture } from "@react-three/drei";
 import * as THREE from "three";
 import { products, ProductId } from "@/data/products";
-
 export type ProductItemProps = {
   productId?: ProductId;
   scale?: number;
 };
-
 export function ProductItem({
   productId = "tshirt",
   scale = 1.2,
@@ -16,25 +13,23 @@ export function ProductItem({
 }: ProductItemProps) {
   const product = products[productId];
   const texture = useTexture(product.image);
-
+  texture.wrapS = THREE.ClampToEdgeWrapping;
+  texture.wrapT = THREE.ClampToEdgeWrapping;
+  texture.minFilter = THREE.LinearFilter;
   return (
     <group {...props} dispose={null} scale={scale}>
-      {/* Front Face */}
       <mesh castShadow receiveShadow>
-        <planeGeometry args={[1, 1.5]} />
-        <meshStandardMaterial 
-          map={texture} 
-          transparent 
-          side={THREE.DoubleSide} 
+        <planeGeometry args={[1, 1.4]} />
+        <meshStandardMaterial
+          map={texture}
+          transparent
+          side={THREE.DoubleSide}
           metalness={0.1}
           roughness={0.8}
         />
       </mesh>
-      
-      {/* Back Face (slightly offset to avoid z-fighting if needed, but DoubleSide is usually fine) */}
-      {/* Adding a bit of "thickness" to make it feel more like a physical card/item if wanted */}
       <mesh position={[0, 0, -0.01]}>
-        <planeGeometry args={[1, 1.5]} />
+        <planeGeometry args={[1, 1.4]} />
         <meshStandardMaterial color={product.accentColor} side={THREE.BackSide} />
       </mesh>
     </group>
